05efa584328a2ab39b40d2bb86d8369203c24c01
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-18 11:22:12 +1100

    First commit

---
+++ enumeratedConstants_008/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace enumeratedConstants_008
+{
+    class Program
+    {
+        enum Color
+        {
+            Red,
+            Orange,
+            Green,
+            Blue,
+            Indigo,
+            Violet,
+        }
+
+        static void Main(string[] args)
+        {
+            Color favorite = Color.Green;
+
+            switch (favorite)
+            {
+                case Color.Red:
+                    Console.WriteLine("You chose Red");
+                    break;
+                case Color.Orange:
+                    Console.WriteLine("You chose Orange");
+                    break;
+                case Color.Green:
+                    Console.WriteLine("You chose Green");
+                    break;
+                case Color.Blue:
+                    Console.WriteLine("You chose Blue");
+                    break;
+                case Color.Indigo:
+                    Console.WriteLine("You chose Indigo");
+                    break;
+                case Color.Violet:
+                    Console.WriteLine("You chose Violet");
+                    break;
+                default:
+                    Console.WriteLine("You didn't chose a color");
+                    break;
+            }
+        }
+    }
+}
